4232c955dd8b25fe393681fcd3cdeda9147276cf
priyanka kariya <priyakariya182@gmail.com>
2026-02-03 18:03:29 +0530

    add_render_details

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "student_attendance_db"; // Make sure this DB exists
+$host = "dpg-d60u1u7gi27c73au7k9g-a";
+$user = "student_attendance_db_r1pu_user";
+$pass = "Xf0dMBMP4ic1Y5JM3uxpwAGzJOTHlbeL";
+$db = "student_attendance_db_r1pu"; // Make sure this DB exists
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
