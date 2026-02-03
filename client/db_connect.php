7d91eb7471579f76473100557e2bbb2fb1677244
priyanka kariya <priyakariya182@gmail.com>
2026-02-03 18:06:49 +0530

    Change DB connection settings for production
    Updated database connection parameters for production.

--- client/db_connect.php
+++ client/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = ""; // Update as needed
-$db = "student_attendance_db";
+$host = "dpg-d60u1u7gi27c73au7k9g-a";
+$user = "student_attendance_db_r1pu_user";
+$pass = "Xf0dMBMP4ic1Y5JM3uxpwAGzJOTHlbeL";
+$db = "student_attendance_db_r1pu"; // Make sure this DB exists
 
 $conn = new mysqli($host, $user, $password, $db);
 if ($conn->connect_error) {
